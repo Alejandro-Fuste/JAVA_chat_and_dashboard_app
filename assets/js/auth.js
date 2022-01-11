c52f17223264585b70b7a03ab0deff2fa7b5da7c
Alejandro F <afuste7@gmail.com>
2022-01-10 20:41:56 -0500

    added role dropdown and changed styling. Added code to retrieve selected value

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -4,10 +4,14 @@ const loginUser = (e) => {
   let userName = document.querySelector("#userName").value.trim();
   let userPassword = document.querySelector("#password").value.trim();
 
+  let role = document.querySelector("#roleSignIn").value;
+
+  let url = `http://127.0.0.1:5000/`;
+
   let loginData = {
     userName,
     userPassword,
   };
 
-  console.table(loginData);
+  console.table(role);
 };
