62858fcd98eaa93be8ca48f1f7ed7ebdeb494831
Alejandro F <afuste7@gmail.com>
2022-01-10 21:31:13 -0500

    added login method

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,3 +1,25 @@
+const login = (data) => {
+  const errorEl = document.querySelector("#hideErrorMessage");
+
+  // send data retrieved from database to localStorage
+  localStorage.setItem("pseudoToken", JSON.stringify(data));
+
+  // redirect user base the role property in data
+  const expr = data.companyRole;
+  switch (expr) {
+    case "business":
+      window.location.assign("/business.html");
+      break;
+    case "business":
+      window.location.assign("/shark.html");
+      break;
+    default:
+      errorEl.setAttribute("id", "errorMessage");
+      errorEl.textContent =
+        "Opps, something went wrong...refresh and try again!";
+  }
+};
+
 const loginUser = async (e) => {
   e.preventDefault();
 
@@ -16,11 +38,10 @@ const loginUser = async (e) => {
   };
 
   // get url depending the role the user selects
-  let url;
+  // let url = "https://58e44f55-bd3b-4e4f-9f73-6396bd1d959b.mock.pstmn.io/";
+  let url = "http://127.0.0.1:5000/";
 
-  role === "Business"
-    ? (url = `http://127.0.0.1:5000/business/login`)
-    : (url = `http://127.0.0.1:5000/shark/login`);
+  role === "Business" ? (url += `business/login`) : (url += `shark/login`);
 
   // use fetch to send login request to server
   const response = await fetch(url, {
@@ -36,13 +57,12 @@ const loginUser = async (e) => {
   // send user to their page if success...display error message if not
   if (response.status === 200) {
     let content = await response.json();
-    login(content);
+    // login(content);
+    console.table(content);
   } else {
     let content = await response.json();
-    console.log(content.message);
+    console.log(content);
     errorEl.setAttribute("id", "errorMessage");
     errorEl.textContent = content.message;
   }
-
-  console.table(url);
 };
