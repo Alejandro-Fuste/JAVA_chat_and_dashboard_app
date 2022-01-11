9a0d673a1a3efec4be02311c878e4a5affaca9e2
Alejandro F <afuste7@gmail.com>
2022-01-11 17:24:19 -0500

    added selectors to get values from biz comment

--- assets/js/data.js
+++ assets/js/data.js
@@ -15,6 +15,18 @@ const sendSharkComment = (e) => {
 
 const sendBusinessComment = (e) => {
   e.preventDefault();
+
+  let reciepient = document.querySelector("#reciepientfromBiz").value.trim();
+  let date = document.querySelector("#commentDateBiz").value.trim();
+  let comment = document.querySelector("#commentBusiness").value.trim();
+
+  let data = {
+    reciepient,
+    date,
+    comment,
+  };
+
+  console.table(data);
 };
 
 const createPitchBusiness = (e) => {
