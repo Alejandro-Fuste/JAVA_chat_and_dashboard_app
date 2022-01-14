5fb96b71a3356131f93756e56876b525422b142c
Alejandro F <afuste7@gmail.com>
2022-01-13 20:13:21 -0500

    removed console logs and commented out code

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,110 +1,78 @@
-// lo({
-//   businessId: 1,
-//   firstName: "Bruce",
-//   lastName: "Wayne",
-//   businessName: "Wayne Enterprises",
-//   username: "Wayne100",
-//   password: "ImBatman",
-//   role: "business",
-// });
+function login(data) {
+  //   e.preventDefault();
+  console.log(data);
+  const errorEl = document.querySelector("#hideErrorMessage");
 
-// const login = (data) => {
-//   e.preventDefault();
-//   console.log(data);
-//   const errorEl = document.querySelector("#hideErrorMessage");
+  // send data retrieved from database to localStorage
+  localStorage.setItem("pseudoToken", JSON.stringify(data));
 
-//   // send data retrieved from database to localStorage
-//   localStorage.setItem("pseudoToken", JSON.stringify(data));
+  // redirect user base the role property in data
+  let expr = data.role;
+  switch (expr) {
+    case "business":
+      location.href = "business.html";
+      break;
+    case "shark":
+      location.href = "shark.html";
+      break;
+    default:
+      errorEl.setAttribute("id", "errorMessage");
+      errorEl.textContent =
+        "Opps, something went wrong...refresh and try again!";
+  }
+}
 
-//   // redirect user base the role property in data
-//   let expr = data.role;
-//   switch (expr) {
-//     case "business":
-//       window.location.assign("/business.html");
-//       break;
-//     case "shark":
-//       window.location.assign("/shark.html");
-//       break;
-//     default:
-//       errorEl.setAttribute("id", "errorMessage");
-//       errorEl.textContent =
-//         "Opps, something went wrong...refresh and try again!";
-//   }
-// };
+async function loginUser2() {
+  // error message selector
+  const errorEl = document.querySelector("#hideErrorMessage");
+
+  // Get username, password, and role from inputs
+  let userName = document.querySelector("#userName").value.trim();
+  let userPassword = document.querySelector("#password").value.trim();
+  let role = document.querySelector("#roleSignIn").value;
+
+  // let arrayData = [userName, userPassword, role];
+
+  // create object for request body
+  let loginData = {
+    username: userName,
+    password: userPassword,
+  };
+
+  //   let url = "https://58e44f55-bd3b-4e4f-9f73-6396bd1d959b.mock.pstmn.io/";
+
+  let url = "http://localhost:8080/";
+
+  //   get url depending the role the user selects
+  role === "Business" ? (url += `business/login`) : (url += `shark/login`);
+
+  // use fetch to send login request to server
+  fetch(url, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(loginData),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      login(data);
+    })
+    .catch((err) => {
+      errorEl.setAttribute("id", "errorMessage");
+      errorEl.textContent = err;
+    });
+}
 
 const logout = () => {
   // delete fake token from local storage
   localStorage.removeItem("pseudoToken");
 
   // redirect user back to home page
-  window.location.href(
-    "file:///Users/alejandro/Desktop/JAVA_investing_app/home.html"
-  );
+  location.href = "home.html";
 };
-
-// const loginUser = async () => {
-//   // error message selector
-//   const errorEl = document.querySelector("#hideErrorMessage");
-
-//   // Get username, password, and role from inputs
-//   let userName = document.querySelector("#userName").value.trim();
-//   let userPassword = document.querySelector("#password").value.trim();
-//   let role = document.querySelector("#roleSignIn").value;
-
-//   let arrayData = [userName, userPassword, role];
-
-//   // create object for request body
-//   let loginData = {
-//     username: userName,
-//     password: userPassword,
-//   };
-
-//   console.log(loginData);
-
-//   // get url depending the role the user selects
-//   // let url = "https://58e44f55-bd3b-4e4f-9f73-6396bd1d959b.mock.pstmn.io/";
-
-//   let url = "http://localhost:8080/";
-
-//   role === "Business" ? (url += `business/login`) : (url += `shark/login`);
-
-//   console.log(url);
-
-//   // use fetch to send login request to server
-//   // fetch(url, {
-//   //   method: "POST",
-//   //   mode: "cors",
-//   //   headers: {
-//   //     Accept: "application/json",
-//   //     "Content-Type": "application/json",
-//   //   },
-//   //   body: JSON.stringify(loginData),
-//   // })
-//   //   .then((response) => response.json())
-//   //   .then((data) => {
-//   //     console.log(data);
-//   //     // login(data);
-//   //   })
-//   //   .catch((err) => {
-//   //     errorEl.setAttribute("id", "errorMessage");
-//   //     errorEl.textContent = err;
-//   //   });
-
-//   let response = await fetch(url, {
-//     method: "POST",
-//     mode: "cors",
-//     headers: { "Content-Type": "application/json" },
-//     body: JSON.stringify(loginData),
-//   });
-//   if (response.status === 200) {
-//     let body = await response.json();
-//     console.log(body);
-//     login(body);
-//   } else {
-//     errorEl.setAttribute("id", "errorMessage");
-//     errorEl.textContent = body;
-//   }
-// };
 
 const createUser = (e) => {
   e.preventDefault();
